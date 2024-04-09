a3b629c2b064a08be5cc754a9f0b6d225cefc594
Vincent Brouwer <dviide@protonmail.com>
2024-04-09 13:55:42 +0200

    full prototype

---
+++ models/product.ts
@@ -0,0 +1,13 @@
+export interface RawProduct {
+  n: string;
+  l: string;
+  p: number;
+  s: string;
+}
+
+export interface Product {
+  name: string;
+  link: string;
+  price: number;
+  size: string;
+}
