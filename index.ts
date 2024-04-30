0506e2688d350715dc124bde613bba256b728d50
Vincent Brouwer <dviide@protonmail.com>
2024-04-30 11:08:54 +0200

    full prototype

--- index.ts
+++ index.ts
@@ -1,53 +1,14 @@
-import Fuse from "fuse.js";
-
+import algoliasearch from "algoliasearch";
 import { fetchSupermarketData } from "./api/api";
-import type { Product } from "./api/models/product";
-import type { Supermarket } from "./api/models/supermarket";
 
-const searchTerm = process.argv[2];
-if (!searchTerm) {
-  throw new Error("please specify a search term");
-}
+const client = algoliasearch(process.env.APPLICATION_ID, process.env.API_KEY);
 
-const cache = await Bun.file("./supermarkets.json");
-const content = await cache.text();
+const supermarkets = await fetchSupermarketData();
+console.log(`Fetched ${supermarkets.length} supermarkets`);
 
-let supermarkets: Supermarket[] = [];
+const index = client.initIndex("supermarkets");
 
-if (content) {
-  supermarkets = JSON.parse(content);
-} else {
-  supermarkets = await fetchSupermarketData();
-  await Bun.write(cache, JSON.stringify(supermarkets));
-}
-
-const allProducts = supermarkets.reduce(
-  (prev, e) => [...prev, ...e.products],
-  [] as Array<Product>
-);
-
-console.log(
-  `searching for ${searchTerm} through ${supermarkets.length} supermarkets and ${allProducts.length} products`
-);
-
-const moddedProducts = allProducts.map((e) => ({
-  uniq: `${e.name} ${e.size} ${e.price}`,
-  name: e.name,
-  link: e.link,
-  price: e.price,
-  size: e.size,
-}));
-
-const fuse = new Fuse(moddedProducts, {
-  keys: ["uniq"],
-});
-
-const results = fuse.search(searchTerm);
-
-console.log("found: ");
-for (let i = 0; i < 3; i++) {
-  const result = results[i];
-  if (!result) continue;
-
-  console.log(result.item.name, result.item.size);
+for (const supermarket of supermarkets) {
+  await index.saveObjects(supermarket.products).wait();
+  console.log(`Saved index for supermarket ${supermarket.name}`);
 }
